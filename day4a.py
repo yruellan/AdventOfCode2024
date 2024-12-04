f5a5f7febf803b75166d16b827db6e467f1c97b7
Yann <ruellan.yann@laposte.net>
2024-12-04 06:32:25 +0100

    Day 4

---
+++ day4a.py
@@ -0,0 +1,51 @@
+
+
+f = open("data4.txt", "r")
+data = f.readlines()
+f.close()
+
+
+s = 0
+print(f"{len(data) = } {len(data[0]) = }")
+
+# Horizontal
+for i in range(len(data)):
+    for j in range(len(data[0])-3):
+        # print(i,j, data[i][j]+data[i][j+1]+data[i][j+2]+data[i][j+3])
+        if data[i][j] == "X" and data[i][j+1] == "M" and data[i][j+2] == "A" and data[i][j+3] == "S":
+            s += 1
+            # print(i, j, 1)
+        if data[i][j] == "S" and data[i][j+1] == "A" and data[i][j+2] == "M" and data[i][j+3] == "X":
+            s += 1
+            # print(i, j, 2)
+
+# Vertical
+for i in range(len(data)-3):
+    for j in range(len(data[0])):
+        # print(i,j, data[i][j]+data[i+1][j]+data[i+2][j]+data[i+3][j])
+        if data[i][j] == "X" and data[i+1][j] == "M" and data[i+2][j] == "A" and data[i+3][j] == "S":
+            s += 1
+            # print(i, j, 3)
+        if data[i][j] == "S" and data[i+1][j] == "A" and data[i+2][j] == "M" and data[i+3][j] == "X":
+            s += 1
+            # print(i, j, 4)
+
+# Diagonal
+for i in range(len(data)-3):
+    for j in range(len(data[0])-3):
+        # print(i,j, data[i][j]+data[i+1][j+1]+data[i+2][j+2]+data[i+3][j+3])
+        if data[i][j] == "X" and data[i+1][j+1] == "M" and data[i+2][j+2] == "A" and data[i+3][j+3] == "S":
+            s += 1
+            # print(i, j, 5)
+        if data[i][j] == "S" and data[i+1][j+1] == "A" and data[i+2][j+2] == "M" and data[i+3][j+3] == "X":
+            s += 1
+            # print(i, j, 6)
+
+        if data[i][j+3] == "X" and data[i+1][j+2] == "M" and data[i+2][j+1] == "A" and data[i+3][j] == "S":
+            s += 1
+            # print(i, j, 7)
+        if data[i][j+3] == "S" and data[i+1][j+2] == "A" and data[i+2][j+1] == "M" and data[i+3][j] == "X":
+            s += 1
+            # print(i, j, 8)
+
+print(s)
---
+++ day4b.py
@@ -0,0 +1,28 @@
+
+
+f = open("data4.txt", "r")
+data = f.readlines()
+f.close()
+
+
+s = 0
+print(f"{len(data) = } {len(data[0]) = }")
+
+def calc_diag_1(data, i, j) -> str:
+    return data[i][j]+data[i+1][j+1]+data[i+2][j+2]
+def calc_diag_2(data, i, j) -> str:
+    return data[i][j+2]+data[i+1][j+1]+data[i+2][j]
+
+for i in range(len(data)-2):
+    for j in range(len(data[0])-3):
+        
+        d1 = calc_diag_1(data, i, j)
+        d2 = calc_diag_2(data, i, j)
+        diag1 = (d1 == "MAS" or d1 == "SAM")
+        diag2 = (d2 == "MAS" or d2 == "SAM")
+
+        # print(i, j, d1,d2)
+        if diag1 and diag2:
+            s += 1
+
+print(s)
