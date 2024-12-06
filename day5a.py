65eefaecd313c88919539b446d36b1609d4e91bc
Yann <ruellan.yann@laposte.net>
2024-12-06 08:42:14 +0100

    Day 5

---
+++ day5a.py
@@ -0,0 +1,34 @@
+
+f = open("data5.txt", "r")
+data = f.read().split("\n\n")
+f.close()
+
+# print(data)
+
+orders = [tuple(map(int,x.split("|")[:2])) for x in data[0].split("\n") ]
+updates = [list(map(int,x.split(","))) for x in data[1].split("\n")[:-1]]
+
+def is_valid(update):
+    for i,x in enumerate(update):
+        for j,y in enumerate(update[i+1:]):
+            if (y,x) in orders:
+                return False
+    return True
+
+def median(l):
+    """Return the median of the list l"""
+    if len(l) % 2 == 0:
+        print("mod 2 : 0")
+        return (l[len(l)//2] + l[len(l)//2-1]) / 2
+    else:
+        return l[len(l)//2]
+
+s = 0
+for update in updates:
+
+    if is_valid(update):
+        m = median(update)
+        s += m
+        print(update, m)
+
+print(s)
---
+++ day5b.py
@@ -0,0 +1,43 @@
+
+f = open("data5.txt", "r")
+data = f.read().split("\n\n")
+f.close()
+
+# print(data)
+
+orders = [tuple(map(int,x.split("|")[:2])) for x in data[0].split("\n") ]
+updates = [list(map(int,x.split(","))) for x in data[1].split("\n")[:-1]]
+
+def is_valid(update):
+    for i,x in enumerate(update):
+        for j,y in enumerate(update[i+1:]):
+            if (y,x) in orders:
+                return False
+    return True
+
+def median(l):
+    """Return the median of the list l"""
+    if len(l) % 2 == 0:
+        print("mod 2 : 0")
+        return (l[len(l)//2] + l[len(l)//2-1]) / 2
+    else:
+        return l[len(l)//2]
+
+def my_sort(l):
+    """Sort the list l"""
+    for i in range(len(l)):
+        for j in range(i+1,len(l)):
+            if (l[j],l[i]) in orders:
+                l[i], l[j] = l[j], l[i]
+
+s = 0
+for update in updates:
+
+    if not is_valid(update):
+        my_sort(update)
+
+        m = median(update)
+        s += m
+        print(update, m)
+
+print(s)
