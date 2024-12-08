d72c0e94ed4a8489dacf7b7e43318dd642b31e4e
Yann <ruellan.yann@laposte.net>
2024-12-08 06:17:56 +0100

    Day 7

---
+++ day7a.py
@@ -0,0 +1,23 @@
+
+f = open("data7.txt", "r")
+data = [
+    (
+        int(x.split(":")[0]), 
+        list(map(int, x.split(":")[1].split()))
+    )
+    for x in f.readlines()
+]
+
+def get_results(nums):
+    if len(nums) <= 1: return nums
+
+    n = nums.pop()
+    results = get_results(nums)
+    return [x + n for x in results] + [x * n for x in results]
+
+s = 0
+for n, nums in data:
+    res = get_results(nums)
+    if n in res: s += n
+
+print(s)
---
+++ day7b.py
@@ -0,0 +1,27 @@
+
+f = open("data7.txt", "r")
+data = [
+    (
+        int(x.split(":")[0]), 
+        list(map(int, x.split(":")[1].split()))
+    )
+    for x in f.readlines()
+]
+
+def concat(a, b):
+    return int(str(a) + str(b))
+
+def get_results(nums):
+    if len(nums) <= 1: return nums
+
+    n = nums.pop()
+    results = get_results(nums)
+    return [x + n for x in results] + [x * n for x in results] + [concat(x, n) for x in results]
+
+s = 0
+for n, nums in data:
+    res = get_results(nums)
+    # print(n in res, res)
+    if n in res: s += n
+
+print(s)
